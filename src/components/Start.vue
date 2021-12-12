060fedf1df539e992af3336e8a25a24b53188d10
Sergio <bostshakur@gmail.com>
2021-12-11 20:25:44 -0500

    Add profile pic

---
+++ src/components/Start.vue
@@ -0,0 +1,22 @@
+<template>
+  <img src="" alt="">
+  <img class="profile" src="../assets/profile.png" alt="">
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+.profile { 
+    width: 15%;
+    border-radius: 50%;
+    border: 4px solid;
+}
+
+</style>
+
+
